56acd0f34dce9702399c36cf4235c44189c0f4af
Naveen Kumar <naveen.s@voonik.com>
2018-04-06 11:34:13 +0530

    redis heroku issue

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
 require "redis"
-conf = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
-$redis = Redis.new(conf.symbolize_keys)
+require 'uri'
+# conf = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
+uri = URI.parse(ENV["REDISTOGO_URL"]||"redis://localhost:6379/")
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# Resque.redis = REDIS
